cbf4667f05265740b35378acbf6e487cbee1c787
jeka <evgeny.chiknaev@gmail.com>
2026-01-30 14:12:09 +0300

    Метод преобразует входную строку в верхний регистр

---
+++ String-kyu/String8-kyu/MakeUpperCase/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println(makeUpperCase("hello world"));
+    }
+
+
+    public static String makeUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}
+
+
+// Напишите функцию, которая преобразует входную строку в верхний регистр.
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/java
