a74b43401c2ff06a5dba6462cd235df314a6f560
jeka <evgeny.chiknaev@gmail.com>
2026-01-31 22:35:29 +0300

    Метод удаляет первый и последний символ, если строка меньше 2 символов возвращает пустую строку

---
+++ String-kyu/String8-kyu/RemoveFirstAndLastCharacter/src/Main.java
@@ -0,0 +1,14 @@
+// Удалить первый и последний символ
+// Задача
+// Ваша задача — написать функцию, которая удаляет первый и последний символы строки. Вам дан один параметр — исходная строка.
+// Важно: Ваша функция должна обрабатывать строки, состоящие из любых length ≥ 2символов. Для строк, содержащих ровно 2символов, возвращайте пустую строку.
+// https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/java
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static String remove(String str) {
+        return str.length() < 2 ? " " : str.substring(1, str.length() - 1);
+    }
+}
