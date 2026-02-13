61754459d0feac4ac7ac7ea545d0c01941218993
jeka <evgeny.chiknaev@gmail.com>
2026-02-13 14:42:19 +0300

    Метод принимает три элемента (массив строк),String,String. Возвращает отформатировынную строку

---
+++ String-kyu/String8-kyu/WelcomeTheCity/src/Main.java
@@ -0,0 +1,16 @@
+//Создайте метод, который принимает на вход имя, город и штат для приветствия человека. Обратите внимание, что name это будет массив,
+// состоящий из одного или нескольких значений, которые должны быть соединены между собой одним пробелом, а длина массива name в тестовых случаях будет варьироваться.
+//https://www.codewars.com/kata/5302d846be2a9189af0001e4/train/java
+
+public class Main {
+    public static void main(String[] args) {
+
+        String [] name = {"John","Smith"};
+        System.out.println(sayHello(name, "Phoenix","Arizona"));
+
+    }
+    public static String sayHello(String [] name, String city, String state){
+        String result = "Hello, " + String.join(" " , name) + "!" + " " + "Welcome to " + city + ", " + state + "!";
+        return result;
+    }
+}
