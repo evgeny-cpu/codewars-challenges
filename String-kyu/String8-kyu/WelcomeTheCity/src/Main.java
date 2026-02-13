d68a34a27d14fcfe04bf1dbeb603141ab12954ec
jeka <evgeny.chiknaev@gmail.com>
2026-02-13 14:48:15 +0300

    Улучшенный метод через регулярное выражение

--- String-kyu/String8-kyu/WelcomeTheCity/src/Main.java
+++ String-kyu/String8-kyu/WelcomeTheCity/src/Main.java
@@ -5,12 +5,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        String [] name = {"John","Smith"};
-        System.out.println(sayHello(name, "Phoenix","Arizona"));
+        String[] name = {"John", "Smith"};
+        System.out.println(sayHello(name, "Phoenix", "Arizona"));
 
     }
-    public static String sayHello(String [] name, String city, String state){
-        String result = "Hello, " + String.join(" " , name) + "!" + " " + "Welcome to " + city + ", " + state + "!";
-        return result;
+
+    public static String sayHello(String[] name, String city, String state) {
+        return String.format("Hello, %s! Welcome to %s, %s!", String.join(" ", name), city, state);
     }
 }
