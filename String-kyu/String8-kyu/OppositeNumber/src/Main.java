5271ae2296da299852bd25b6eefbd2479ffdff40
jeka <evgeny.chiknaev@gmail.com>
2026-03-13 22:18:23 +0300

    Метод возвращает противоположное число

---
+++ String-kyu/String8-kyu/OppositeNumber/src/Main.java
@@ -0,0 +1,14 @@
+//Very simple, given a number, find its opposite (additive inverse).
+//https://www.codewars.com/kata/56dec885c54a926dcd001095/train/java
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println(opposite(-34));
+    }
+
+
+    public static int opposite(int number) {
+        return -number;
+    }
+}
