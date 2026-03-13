d6a9d27b70b149ba6d74a40e2e5ee61f41cc7ec2
jeka <evgeny.chiknaev@gmail.com>
2026-03-13 23:03:50 +0300

    метод чистит строку оставляя только гласные буквы и подсчитывает длину строки

---
+++ Kata_7_kyu/VowelCount/src/Main.java
@@ -0,0 +1,16 @@
+//Return the number (count) of vowels in the given string.
+//We will consider a, e, i, o, u as vowels for this Kata (but not y).
+//The input string will only consist of lower case letters and/or spaces.
+//https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/java
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(getCount("o a kak ushakov lil vo kashu kakao"));
+    }
+
+    public static int getCount(String str) {
+        String result = str.replaceAll("[^aeiou]", "");
+        return result.length();
+    }
+
+}
