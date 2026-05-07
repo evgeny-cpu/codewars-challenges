da2f81d40b38d7369b39fd0fcf6dc34abb196cdb
jeka <evgeny.chiknaev@gmail.com>
2026-05-07 12:32:55 +0300

    метод проверяет на четность число

---
+++ String-kyu/String8-kyu/EvenOrOdd/src/Main.java
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/java
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static String evenOrOdd(int number) {
+        return number % 2 == 0 ? "Even" : "Odd";
+    }
+
+}
