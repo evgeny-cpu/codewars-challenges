0f5b09af81779279c4ec76ae6fa732264b18abed
jeka <evgeny.chiknaev@gmail.com>
2026-05-14 11:38:37 +0300

    метод рассчитывает время

---
+++ String-kyu/String8-kyu/BeginnerSeriesClock/src/Main.java
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/java
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println(Past(14,20,31));
+    }
+
+    public static int Past(int h, int m, int s) {
+            return ((h * 60 + m) * 60  + s) * 1000;
+    }
+}
