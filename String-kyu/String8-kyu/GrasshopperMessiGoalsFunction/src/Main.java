79b58d6cd9b04c43e4023621011bb58d9fac2651
jeka <evgeny.chiknaev@gmail.com>
2026-05-17 11:15:56 +0300

    calculated from goals

---
+++ String-kyu/String8-kyu/GrasshopperMessiGoalsFunction/src/Main.java
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/java
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println(goals(43,10,5));
+    }
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+
+}
