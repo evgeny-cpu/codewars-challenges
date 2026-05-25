94dbbcdef57d4cda4f4560fbcaef3515cfa6b537
jeka <evgeny.chiknaev@gmail.com>
2026-05-25 10:59:44 +0300

    метод возвращает строкой true and false

---
+++ String-kyu/String8-kyu/ConvertBooleanValuesToStringsYesOrNo/src/Main.java
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/53369039d7ab3ac506000467/train/java
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println(boolToWord(true));
+    }
+
+    public static String boolToWord(boolean b) {
+        return b ? "Yes" : "No";
+    }
+}
