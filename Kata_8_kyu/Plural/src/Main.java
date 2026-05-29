9c35f6ecf354d7e7313c418a8e4e3106a42db3ca
jeka <evgeny.chiknaev@gmail.com>
2026-05-29 23:29:12 +0300

    метод возвращает не множественное значение

---
+++ Kata_8_kyu/Plural/src/Main.java
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/52ceafd1f235ce81aa00073a/train/java
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println(isPlural(1f));
+    }
+
+    public static boolean isPlural(float f) {
+        return f != 1;
+    }
+}
