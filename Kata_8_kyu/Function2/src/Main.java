6c1504924777e959360b0efac49b5f6f84cf459e
jeka <evgeny.chiknaev@gmail.com>
2026-05-30 16:55:07 +0300

    метод возвращает крадрат

---
+++ Kata_8_kyu/Function2/src/Main.java
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/523b623152af8a30c6000027/train/java
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println(square(5));
+    }
+
+    public static int square(int n) {
+        return n * n;
+    }
+}
