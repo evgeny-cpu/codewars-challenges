7fa73706c1ce872f3f2d3ec8512630083687e56e
jeka <evgeny.chiknaev@gmail.com>
2026-06-06 19:05:28 +0300

    метод вычисляет нечетное число

---
+++ Kata_8_kyu/OppositesAttract/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println(isLove(2, 2));
+    }
+
+    public static boolean isLove(final int flower1, final int flower2) {
+        return (flower1 + flower2) % 2 != 0;
+    }
+}
