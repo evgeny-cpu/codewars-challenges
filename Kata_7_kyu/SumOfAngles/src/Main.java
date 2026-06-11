b324c644a3df1c7ad9bf2cf7da0db864bcc3ddd7
jeka <evgeny.chiknaev@gmail.com>
2026-06-11 23:21:31 +0300

    метод находит сумму внутренних углов

---
+++ Kata_7_kyu/SumOfAngles/src/Main.java
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/5a03b3f6a1c9040084001765/train/java
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println(sumOfAngles(4));
+    }
+
+    public static int sumOfAngles(int n) {
+        return (n - 2) * 180;
+    }
+}
