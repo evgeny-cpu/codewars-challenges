00811fafee3cbdc9b611b44755e3739e31bb9146
jeka <evgeny.chiknaev@gmail.com>
2026-06-17 17:09:56 +0300

    подсчёт забитых голов за этот год

---
+++ Kata_8_kyu/GrasshopperMessiGoals/src/Main.java
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/55ca77fa094a2af31f00002a/train/java
+public class Main {
+    public static int laLigaGoals = 43;
+    public static int championsLeagueGoals = 10;
+    public static int copaDelReyGoals = 5;
+
+    public static int totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
+
+    public static void main(String[] args) {
+
+        System.out.println(totalGoals );
+    }
+
+}
